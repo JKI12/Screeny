4a7ef6c2ff6e948b6c5bee5042362afe25a6bb21
King, Jake (Apprentice Software Developer) <Jake.King@sky.uk>
2018-01-21 21:21:14 +0000

    removed nightmare screenshots

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,6 @@ import koaStatic from 'koa-static';
 import koaCors from 'koa2-cors';
 import path from 'path';
 
-import { getImage } from './screenshot';
-
 const app = new koa();
 const router = new koaRouter();
 
@@ -16,29 +14,6 @@ app.use(koaCors());
 const clientPath = path.join(__dirname, '..', 'client');
 
 app.use(koaStatic(clientPath));
-
-router.get('/screenshot', async (ctx) => {
-  const url = ctx.query.url;
-  
-  if (!url) {
-    ctx.status = 400;
-    return ctx.body = 'No Url Provided';
-  }
-
-  const regex = /^https?:///;
-
-  if (!regex.test(url)) {
-    ctx.status = 400;
-    return ctx.body = 'Invalid Url Provided';
-  }
-
-  const image = await getImage(url);
-
-  ctx.body = {
-    url,
-    image
-  };
-});
 
 router.get('/sites', (ctx) => {
   ctx.body = require(path.join(__dirname, '../../', 'static', 'sites.json'));
